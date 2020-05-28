2107d0c6f62a36571ab857180ed35a1d73d33e8b
Jake Morrison <jake@cogini.com>
2020-05-28 11:38:02 +0800

    Generate project

---
+++ lib/ecto_extract_migrations.ex
@@ -0,0 +1,18 @@
+defmodule EctoExtractMigrations do
+  @moduledoc """
+  Documentation for `EctoExtractMigrations`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoExtractMigrations.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_extract_migrations_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoExtractMigrationsTest do
+  use ExUnit.Case
+  doctest EctoExtractMigrations
+
+  test "greets the world" do
+    assert EctoExtractMigrations.hello() == :world
+  end
+end
