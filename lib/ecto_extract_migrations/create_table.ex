c1a0ed6e5b1b16a149a04316d3d8d765235798e7
Jake Morrison <jake@cogini.com>
2020-05-29 11:17:12 +0800

    Use parser combinators

---
+++ lib/ecto_extract_migrations/create_table.ex
@@ -0,0 +1,33 @@
+defmodule EctoExtractMigrations.CreateTable do
+  import NimbleParsec
+
+  # https://www.postgresql.org/docs/current/sql-createtable.html
+
+  # https://www.postgresql.org/docs/current/sql-syntax-lexical.html
+  whitespace = ascii_char([32, ?\t])
+               |> times(min: 1)
+
+  # identifier = utf8_string([], min: 1)
+  identifier =utf8_string([?a..?z, ?A..?Z, ?0..?9], min: 1)
+
+  quoted_identifier = ignore(ascii_char([?"]))
+                      |> concat(identifier)
+                      |> ignore(ascii_char([?"]))
+
+  name = choice([identifier, quoted_identifier])
+
+  schema_name = name
+  table_name = name
+
+  schema_qualified_table_name = schema_name |> ascii_char([?.]) |> concat(table_name)
+  
+  create_table = string("CREATE")
+           |> ignore(whitespace)
+           |> string("TABLE")
+           |> ignore(whitespace)
+
+  defparsec :parse, ignore(create_table)
+  |> choice([schema_qualified_table_name, table_name])
+
+  defparsec :parse_table_name, choice([schema_qualified_table_name, table_name])
+end
