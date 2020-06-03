ba4035b99b74b697615c7446a2009cd9450f7ae8
Jake Morrison <jake@cogini.com>
2020-06-03 11:26:36 +0800

    Test format_table_name

--- test/ecto_extract_migrations_test.exs
+++ test/ecto_extract_migrations_test.exs
@@ -1,4 +1,12 @@
 defmodule EctoExtractMigrationsTest do
   use ExUnit.Case
 
+  import EctoExtractMigrations
+
+  test "format_table_name/1" do
+    assert "mytable" == format_table_name("mytable")
+    assert "myschema.mytable" == format_table_name(["myschema", "mytable"])
+    assert "mytable" == format_table_name(["public", "mytable"])
+  end
+
 end
