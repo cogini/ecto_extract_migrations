46e3c9bfa3aae1a0b8fbb0ac61b0f87698b605f4
Jake Morrison <jake@cogini.com>
2020-06-04 11:16:22 +0800

    Improve CREATE INDEX parser

---
+++ lib/ecto_extract_migrations/index.ex
@@ -0,0 +1,76 @@
+defmodule EctoExtractMigrations.Index do
+  @app :ecto_extract_migrations
+
+  def create_migration(data, bindings) do
+    [schema, table] = data.table_name
+
+    table_name = ~s|"#{table}"|
+
+    prefix =
+      if schema == "public" do
+        nil
+      else
+        schema
+      end
+
+    opts = [ 
+      name: data[:name],
+      unique: data[:unique],
+      concurrently: data[:concurrently],
+      using: data[:using],
+      prefix: prefix,
+      where: data[:where],
+      include: data[:include],
+    ]
+    
+   opts = opts
+      |> Enum.reject(&nil_value/1)
+      |> Enum.map(&format_opt/1)
+      |> Enum.join(", ")
+
+    index_args = Enum.join([table_name, format_key(data.key)] ++ [opts], ", ")
+
+    # :name - the name of the index. Defaults to "#{table}_#{column}_index".
+    # :unique - indicates whether the index should be unique. Defaults to false.
+    # :concurrently - indicates whether the index should be created/dropped concurrently.
+    # :using - configures the index type.
+    # :prefix - specify an optional prefix for the index.
+    # :where - specify conditions for a partial index.
+    # :include - specify fields for a covering index. This is not supported by all databases. For more information on PostgreSQL support, please read the official docs.
+
+    bindings = Keyword.merge(bindings, [
+      module_name: EctoExtractMigrations.format_module_name(data.name),
+      index_args: index_args,
+    ])
+
+    template_dir = Application.app_dir(@app, ["priv", "templates"])
+    template_path = Path.join(template_dir, "index.eex")
+    EctoExtractMigrations.eval_template(template_path, bindings)
+  end
+
+  def nil_value({_, nil}), do: true
+  def nil_value(_), do: false
+
+  def format_opt({key, value}) when is_binary(value) do
+    value = escape(value)
+    if String.contains?(value, ~s(")) do
+      ~s|#{key}: """\n#{value}\n"""|
+    else
+      ~s|#{key}: "#{value}"|
+    end
+  end
+  def format_opt({key, value}), do: "#{key}: #{value}"
+
+  def format_key(values) do
+    value =
+      values
+      |> Enum.map(&format_column/1)
+      |> Enum.join(", ")
+    "[" <> value <> "]"
+  end
+
+  def escape(value), do: String.replace(value, "\\", "\\\\")
+
+  def format_column(value) when is_atom(value), do: ~s|:#{value}|
+  def format_column(value) when is_binary(value), do: ~s|"#{value}"|
+end
--- lib/ecto_extract_migrations/parsers/create_index.ex
+++ lib/ecto_extract_migrations/parsers/create_index.ex
@@ -14,6 +14,8 @@ defmodule EctoExtractMigrations.Parsers.CreateIndex do
   # [ WHERE predicate ]
 
   whitespace = Common.whitespace()
+  identifier = Common.identifier()
+  name = Common.name()
 
   table_name =
     Common.table_name(:table_name)
@@ -21,16 +23,47 @@ defmodule EctoExtractMigrations.Parsers.CreateIndex do
   index_name =
     Common.table_name(:name)
 
+  column_name = Common.column_name() |> post_traverse(:to_atom) |> label("column_name")
+
+  defp to_atom(_rest, acc, context, _line, _offset) do
+    atoms = Enum.map(acc, &String.to_atom/1)
+    {atoms, context}
+  end
+
+  defp wrap_parens(_rest, acc, context, _line, _offset) do
+    values = Enum.map(acc, &("(" <> &1 <> ")"))
+    {values, context}
+  end
+
+  defp unwrap_parens_traverse(_rest, acc, context, _line, _offset) do
+    values = Enum.map(acc, &unwrap_parens/1)
+    {values, context}
+  end
+
+  # defp unwrap_parens_traverse(_rest, acc, context, _line, _offset) do
+  #   values = Enum.map(acc, &unwrap_parens/1)
+  #   {values, context}
+  # end
+
+  def unwrap_parens(value) do
+    String.replace_prefix(value, "(", "") |> String.replace_suffix(")", "")
+  end
+
   unique =
     ignore(optional(whitespace))
     |> string("UNIQUE")
+    |> replace(true)
+    |> unwrap_and_tag(:unique)
 
   concurrently =
     ignore(optional(whitespace))
     |> string("CONCURRENTLY")
+    |> replace(true)
+    |> unwrap_and_tag(:concurrently)
 
   if_not_exists =
-    string("IF NOT EXISTS") |> ignore(whitespace)
+    ignore(optional(whitespace))
+    |> string("IF NOT EXISTS")
 
   only =
     ignore(optional(whitespace))
@@ -43,16 +76,89 @@ defmodule EctoExtractMigrations.Parsers.CreateIndex do
     |> concat(Common.name())
     |> unwrap_and_tag(:using)
 
-  key =
+  lparen = ascii_char([?(]) |> label("(")
+  rparen = ascii_char([?)]) |> label(")")
+
+  expression =
+    utf8_string([?a..?z, ?A..?Z, ?0..?9, ?_, ?=, ?>, ?<, ?\s, ?', ?-, ?:], min: 1)
+    |> post_traverse(:wrap_parens)
+    |> label("expression")
+
+  sql_expression =
+    # utf8_string([?a..?z, ?A..?Z, ?0..?9, ?_, ?=, ?>, ?<, ?\s, ?', ?-, ?:, ?(, ?)], min: 1)
     utf8_string([{:not, ?;}], min: 1)
-    |> unwrap_and_tag(:key)
+    |> label("sql_expression")
+
+  defcombinatorp(:expr,
+    ignore(lparen)
+    |> choice([parsec(:expr), expression])
+    |> ignore(rparen)
+    |> label("expr")
+  )
+
+  column =
+    choice([column_name, parsec(:expr)])
+
+  key =
+    ignore(lparen)
+    |> times(column |> ignore(optional(ascii_char([?,]))) |> ignore(optional(whitespace)), min: 1)
+    |> ignore(rparen)
+    |> tag(:key)
+    |> label("key")
+
+  include =
+    ignore(optional(whitespace))
+    |> ignore(string("INCLUDE"))
+    |> ignore(whitespace)
+    |> ignore(lparen)
+    |> times(column |> ignore(optional(ascii_char([?,]))) |> ignore(optional(whitespace)), min: 1)
+    |> ignore(rparen)
+    |> tag(:include)
+    |> label("include")
+
+  equal_sign = ascii_char([?=]) |> label("=")
+  semicolon = ascii_char([?;]) |> label(";")
+
+  storage_parameter =
+    identifier
+    |> concat(whitespace)
+    |> concat(equal_sign)
+    |> concat(whitespace)
+    |> concat(identifier)
+    |> label("storage_parameter")
+
+  with_ =
+    ignore(optional(whitespace))
+    |> ignore(string("WITH"))
+    |> ignore(whitespace)
+    |> ignore(lparen)
+    |> times(storage_parameter |> ignore(optional(ascii_char([?,]))) |> ignore(optional(whitespace)), min: 1)
+    |> ignore(rparen)
+    |> tag(:with)
+    |> label("with")
+
+  tablespace =
+    ignore(optional(whitespace))
+    |> ignore(string("TABLESPACE"))
+    |> ignore(whitespace)
+    |> concat(name)
+    |> unwrap_and_tag(:tablespace)
+    |> label("tablespace")
+
+  where =
+    ignore(optional(whitespace))
+    |> ignore(string("WHERE"))
+    |> ignore(whitespace)
+    |> concat(sql_expression) |> post_traverse(:unwrap_parens_traverse)
+    |> unwrap_and_tag(:where)
+    |> label("where")
 
   create_index =
     ignore(string("CREATE"))
-    |> ignore(optional(unique))
+    |> optional(unique)
     |> ignore(whitespace)
     |> ignore(string("INDEX"))
-    |> ignore(optional(concurrently))
+    |> optional(concurrently)
     |> ignore(optional(if_not_exists))
     |> ignore(whitespace)
     |> concat(index_name)
@@ -64,29 +170,31 @@ defmodule EctoExtractMigrations.Parsers.CreateIndex do
     |> optional(using)
     |> ignore(whitespace)
     |> concat(key)
-    |> ignore(string(";"))
+    |> optional(include)
+    |> optional(with_)
+    |> optional(tablespace)
+    |> optional(where)
+    |> ignore(semicolon)
     |> ignore(optional(whitespace))
     |> reduce({Enum, :into, [%{}]})
 
   defparsec :parsec_create_index, create_index
 
+
+  # :name - the name of the index. Defaults to "#{table}_#{column}_index".
+  # :unique - indicates whether the index should be unique. Defaults to false.
+  # :concurrently - indicates whether the index should be created/dropped concurrently.
+  # :using - configures the index type.
+  # :prefix - specify an optional prefix for the index.
+  # :where - specify conditions for a partial index.
+  # :include - specify fields for a covering index. This is not supported by all databases. For more information on PostgreSQL support, please read the official docs.
+
   def parse(sql) do
     case parsec_create_index(sql) do
       {:ok, [value], _, _, _, _} ->
-        value = update_in(value[:key], &fix_key/1) 
         {:ok, value}
       error -> error
     end
-  end
-
-  def unwrap_parens(value) do
-    String.replace_prefix(value, "(", "") |> String.replace_suffix(")", "")
-  end
-
-  def fix_key(value) do
-    value
-    |> unwrap_parens
-    |> String.split(~r/,\s+/)
   end
 
 end
