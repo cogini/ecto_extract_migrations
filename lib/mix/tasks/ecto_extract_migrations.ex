e9c7004d54c17cf14635be36cd9ba493c0d4e304
Jake Morrison <jake@cogini.com>
2020-06-04 18:51:04 +0800

    Handle types in order

--- lib/mix/tasks/ecto_extract_migrations.ex
+++ lib/mix/tasks/ecto_extract_migrations.ex
@@ -15,6 +15,8 @@ defmodule Mix.Tasks.Ecto.Extract.Migrations do
   """
   @shortdoc "Initialize template files"
 
+  @app :ecto_extract_migrations
+
   alias EctoExtractMigrations.Index
   alias EctoExtractMigrations.Schema
   alias EctoExtractMigrations.Sequence
@@ -38,11 +40,9 @@ defmodule Mix.Tasks.Ecto.Extract.Migrations do
     {overrides, _} = OptionParser.parse!(args, opts)
 
     repo = overrides[:repo] || "Repo"
-    repo_dir = Macro.underscore(repo)
-    default_migrations_path = Path.join(["priv", repo_dir, "migrations"])
-    migrations_path = overrides[:migrations_path] || default_migrations_path
     sql_file = overrides[:sql_file]
 
+    migrations_path = get_migrations_path(overrides)
     :ok = File.mkdir_p(migrations_path)
 
     results =
@@ -53,34 +53,107 @@ defmodule Mix.Tasks.Ecto.Extract.Migrations do
       |> Stream.map(&parse_sql/1)
       |> Enum.to_list
 
+    # TODO
+    # CREATE TABLE
+    #   Parse CONSTRAINTS with new expression parser
+    #   Column options are not in order, use choice
+    #     e.g. public.login_log
+    #   Handle column constraints
+    #
+    # ALTER TABLE
+    #   ALTER TABLE ONLY chat.assignment ALTER COLUMN id SET DEFAULT nextval
+    #   ALTER TABLE ONLY chat.session ADD CONSTRAINT session_token_key UNIQUE (token);
+    #   ALTER TABLE ONLY chat.assignment ADD CONSTRAINT assignment_care_taker_id_fkey FOREIGN KEY (user_id) REFERENCES chat."user"(id);
+    #
+    #   Merge with create table
+    #
+    # CREATE INDEX
+    #   Consolidate statements for performance
+    #
+    # CREATE FUNCTION
+    # CREATE TRIGGER
+    # CREATE EXTENSION
+    #   CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
+    #   COMMENT ON EXTENSION pg_stat_statements IS 'track execution statistics of all SQL statements executed';
+
+    index = 1
+
     bindings = [
       repo: repo,
     ]
 
-    # TODO
-    # merge alter table with table
-    # constraints
-    # create index
-    # unique constraints
-    # CREATE TABLE column options are not in order, use choice
-    # Handle column constraints
-    # Handle ALTER TABLE SET DEFAULT
-    # Handle ALTER TABLE UNIQUE CONSTRAINT
-    # Handle ALTER TABLE FOREIGN KEY CONSTRAINT
-    # Consolidate CREATE INDEX statements for performance
-    # Handle CREATE FUNCTION
-    # Handle CREATE TRIGGER
+    # Create schemas
+    {schemas, results} = Enum.split_with(results, fn result -> result.type == :create_schema end)
+    for {%{type: type, sql: sql, data: data, idx: idx}, index} <- Enum.with_index(schemas, index) do
+      Mix.shell().info("SQL #{type} #{idx} \n#{sql}\n#{inspect data}")
+      prefix = to_string(:io_lib.format('~4..0b', [index]))
+      data = Map.put(data, :sql, sql)
 
+      {:ok, migration} = Schema.create_migration(data, bindings)
+      filename = Path.join(migrations_path, Schema.migration_filename(prefix, data))
+      write_migration_file(migration, filename)
+    end
+    index = index + length(schemas)
+
+    # Create sequences
+    {sequences, results} = Enum.split_with(results, &(&1.type == :create_sequence))
+    statements = for result <- sequences do
+      [schema, name] = result.data.name
+      Sequence.create_migration_statement(result.sql, schema, name)
+    end
+    {:ok, migration} = create_sequences_migration(statements, bindings)
+    prefix = to_string(:io_lib.format('~4..0b', [index]))
+    filename = Path.join(migrations_path, "#{prefix}_sequences.exs")
+    write_migration_file(migration, filename)
+    index = index + 1
+
+    # Create types
+    {types, results} = Enum.split_with(results, &(&1.type == :create_type))
+    for {%{type: type, sql: sql, data: data, idx: idx}, index} <- Enum.with_index(types, index + 1) do
+      Mix.shell().info("SQL #{type} #{idx} \n#{sql}\n#{inspect data}")
+      prefix = to_string(:io_lib.format('~4..0b', [index]))
+      data = Map.put(data, :sql, sql)
+
+      {:ok, migration} = Type.create_migration(data, bindings)
+      filename = Path.join(migrations_path, Type.migration_filename(prefix, data))
+      write_migration_file(migration, filename)
+    end
+    index = index + length(types)
+
+    # Collect table primary_keys from ALTER TABLE statements
+    {alter_table_primary_keys, results} = Enum.split_with(results, &is_alter_table_pk_constraint/1)
     primary_keys =
-      for result <- results, is_pk_constraint(result), into: %{} do
+      for result <- alter_table_primary_keys, into: %{} do
         {result.data.table, result.data.primary_key}
       end
 
-    table_constraints =
-      Enum.flat_map(results, &get_table_constraints/1)
+    # Collect table defaults from ALTER TABLE statements
+    {alter_table_set_defaults, results} = Enum.split_with(results, &is_alter_table_set_default/1)
+    column_defaults =
+      for result <- alter_table_set_defaults, reduce: %{} do
+        acc ->
+          %{table: table, column: column, default: default} = result.data
+          value = acc[table] || %{}
+          Map.put(acc, table, Map.put(value, column, default))
+      end
+
+    {references, results} = Enum.split_with(results,
+      &(&1.action == :add_table_constraint and &1.type == :foreign_key))
+    Mix.shell().info("references: #{inspect references}")
+
+    # table
+    # :name - The name of the underlying reference, which defaults to "#{table}_#{column}_fkey".
+    # :column - The foreign key column name, which defaults to :id.
+    # :prefix - The prefix for the reference. Defaults to the prefix defined by the block's table/2 struct (the "products" table in the example above), or nil.
+    # :type - The foreign key type, which defaults to :bigserial.
+    # :on_delete - What to do if the referenced entry is deleted. May be :nothing (default), :delete_all, :nilify_all, or :restrict.
+    # :on_update - What to do if the referenced entry is updated. May be :nothing (default), :update_all, :nilify_all, or :restrict.
+
+    # Collect table constraints
+    table_constraints = Enum.flat_map(results, &get_table_constraints/1)
     Mix.shell().info("table_constraints: #{inspect table_constraints}")
 
-    for {%{type: type, sql: sql, data: data, idx: idx}, index} <- Enum.with_index(results) do
+    for {%{type: type, sql: sql, data: data, idx: idx}, index} <- Enum.with_index(results, index + 1) do
       Mix.shell().info("SQL #{type} #{idx} \n#{sql}\n#{inspect data}")
       prefix = to_string(:io_lib.format('~4..0b', [index]))
       data = Map.put(data, :sql, sql)
@@ -91,60 +164,32 @@ defmodule Mix.Tasks.Ecto.Extract.Migrations do
             Mix.shell().info("Skipping schema_migrations")
             :ok
           else
-            [schema, name] = data.name
-            data = table_set_pk(data, primary_keys[data.name])
+            data =
+              data
+              |> table_set_pk(primary_keys[data.name])
+              |> table_set_default(column_defaults[data.name])
+
             {:ok, migration} = Table.create_migration(data, bindings)
-            filename = Path.join(migrations_path, "#{prefix}_table_#{schema}_#{name}.exs")
-            Mix.shell().info(filename)
-            Mix.shell().info(migration)
-            :ok = File.write(filename, migration)
-
-            # constraints = data[:constraints] || []
-            # if not Enum.empty?(constraints) do
-            #   constraint_data = %{table: data.name, constraints: constraints}
-            #   {:ok, migration} = Constraint.create_migration(constraint_data, bindings)
-            #   filename = Path.join(migrations_path, "#{prefix}1_constraint_#{schema}_#{name}.exs")
-            #   Mix.shell().info(filename)
-            #   Mix.shell().info(migration)
-            #   :ok = File.write(filename, migration)
-            # end
+            filename = Path.join(migrations_path, Table.migration_filename(prefix, data))
+            write_migration_file(migration, filename)
           end
-        :create_schema ->
-          :ok = create_migration(Schema, data, bindings,
-            Path.join(migrations_path, "#{prefix}_schema_#{data.name}.exs"))
-        :create_type ->
-          name = Enum.join(data.name, "_")
-          :ok = create_migration(Type, data, bindings,
-            Path.join(migrations_path, "#{prefix}_type_#{name}.exs"))
         :create_view ->
-          name = Enum.join(data.name, "_")
-          :ok = create_migration(View, data, bindings,
-            Path.join(migrations_path, "#{prefix}_index_#{name}.exs"))
-
-          # {:ok, migration} = View.create_migration(Map.put(data, :sql, sql), bindings)
-          # [schema, name] = data.name
-          # filename = Path.join(migrations_path, "#{prefix}_view_#{schema}_#{name}.exs")
-          # Mix.shell().info(filename)
-          # Mix.shell().info(migration)
-          # :ok = File.write(filename, migration)
+          {:ok, migration} = View.create_migration(data, bindings)
+          filename = Path.join(migrations_path, View.migration_filename(prefix, data))
+          write_migration_file(migration, filename)
         :create_index ->
-          :ok = create_migration(Index, data, bindings,
-            Path.join(migrations_path, "#{prefix}_index_#{data.name}.exs"))
-        :create_sequence ->
-          :ok = create_migration(Sequence, data, bindings,
-            Path.join(migrations_path, "#{prefix}_sequence_#{data.name}.exs"))
+          {:ok, migration} = Index.create_migration(data, bindings)
+          filename = Path.join(migrations_path, Index.migration_filename(prefix, data))
+          write_migration_file(migration, filename)
+        # :create_sequence ->
+          # {:ok, migration} = Sequence.create_migration(data, bindings)
+          # filename = Path.join(migrations_path, Sequence.migration_filename(prefix, data))
+          # write_migration_file(migration, filename)
         :alter_table ->
           :ok
       end
     end
 
-  end
-
-  def create_migration(module, data, bindings, filename) do
-    {:ok, migration} = apply(module, :create_migration, [data, bindings])
-    Mix.shell().info(filename)
-    Mix.shell().info(migration)
-    :ok = File.write(filename, migration)
   end
 
   @doc "Extract SQL for statements from file"
@@ -197,9 +242,36 @@ defmodule Mix.Tasks.Ecto.Extract.Migrations do
   def sql_parser(:create_type), do: &EctoExtractMigrations.Parsers.CreateType.parse/1
   def sql_parser(:create_view), do: &EctoExtractMigrations.Parsers.CreateView.parse/1
 
+  def get_migrations_path(overrides) do
+    repo = overrides[:repo] || "Repo"
+    repo_dir = Macro.underscore(repo)
+    default_migrations_path = Path.join(["priv", repo_dir, "migrations"])
+    overrides[:migrations_path] || default_migrations_path
+  end
+
+  def write_migration_file(migration, filename) do
+    Mix.shell().info(filename)
+    Mix.shell().info(migration)
+    :ok = File.write(filename, migration)
+  end
+
+  def get_sequence_statements(results) do
+    for result <- results, result.type == :create_sequence do
+      [schema, name] = result.data.name
+      Sequence.create_migration_statement(result.sql, schema, name)
+    end
+  end
+
+  def create_sequences_migration(statements, bindings) do
+    bindings = Keyword.merge([module_name: "Sequences", sequences: statements], bindings)
+    template_dir = Application.app_dir(@app, ["priv", "templates"])
+    template_path = Path.join(template_dir, "sequences.eex")
+    EctoExtractMigrations.eval_template(template_path, bindings)
+  end
+
   # %{action: :add_constraint, constraint_name: "message_pkey", primary_key: ["id"], table: ["chat", "message"]}
-  def is_pk_constraint(%{type: :alter_table, data: %{action: :add_constraint, primary_key: _pk}}), do: true
-  def is_pk_constraint(_), do: false
+  def is_alter_table_pk_constraint(%{type: :alter_table, data: %{action: :add_constraint, primary_key: _pk}}), do: true
+  def is_alter_table_pk_constraint(_), do: false
 
   # Set primary_key: true on column if it is part of table primary key
   def table_set_pk(data, nil), do: data
@@ -218,6 +290,27 @@ defmodule Mix.Tasks.Ecto.Extract.Migrations do
       Map.put(column, :primary_key, true)
     else
       column
+    end
+  end
+
+
+  def is_alter_table_set_default(%{type: :alter_table, data: %{action: :set_default}}), do: true
+  def is_alter_table_set_default(_), do: false
+
+  # Set default on column based on alter table
+  def table_set_default(data, nil), do: data
+  def table_set_default(data, defaults) do
+    Mix.shell().info("setting default: #{inspect data.name} #{inspect defaults}")
+    columns = Enum.map(data[:columns], &(column_set_default(&1, defaults)))
+    %{data | columns: columns}
+  end
+
+  def column_set_default(data, defaults) do
+    case Map.fetch(defaults, data.name) do
+      {:ok, default} ->
+        Map.put(data, :default, default)
+      :error ->
+        data
     end
   end
 
