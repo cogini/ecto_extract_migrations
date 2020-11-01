5b9133010c71568fb8495c81aa40104f0779c404
Jake Morrison <jake@cogini.com>
2020-11-01 09:40:25 +0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule EctoExtractMigrations.MixProject do
   def project do
     [
       app: :ecto_extract_migrations,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
